cc6e44343a57d59c3a4aba4ab912a608f933778c
anagha7 <ANAGHA31997@GMAIL.COM>
2019-02-25 14:56:20 +0530

    hello

--- __init__.py
+++ __init__.py
@@ -1,8 +1,35 @@
-from  flask import Flask
-
+from  flask import Flask,render_template,request
+from flask_socketio import SocketIO,send,emit
 app=Flask(__name__)
+app.config['SECRET_KEY']="mysecretkey"
+#from app import routes
 
-from app import routes
+sock=SocketIO(app)
+
+@sock.on('message')
+def handle_message(msg):
+
+    print('message : '+msg)
+    sock.send(msg,broadcast=True)
+
+@sock.on('usermessage',namespace='/public')
+def public_message(msg):
+    print('public message : '+msg)
+    emit('fromflask',msg,broadcast=True)
+
+@sock.on('username',namespace='/private')
+def rcv_username(user):
+    print(user+request.sid)
+   # emit('fromflask',msg,broadcast=True)
+
+@app.route('/')
+def chat():
+
+    return render_template('chat2.html')
+
+@app.route('/chat')
+def chattem():
+    return render_template('chattem.html')
 
 if __name__ == "__main__" :
-    app.run(debug='true')
+    sock.run(app,debug='true')
--- routes.py
+++ routes.py
@@ -4,7 +4,8 @@ from flask import render_template
 
 @app.route('/')
 def chat():
-    return render_template('chats1.html')
+
+    return render_template('chats2.html')
 
 @app.route('/chat')
 def chattem():
@@ -16,9 +17,9 @@ def login(username,password):
 
     return render_template('login.html')
 
-@app.route('/register')
-def reg(name,birtday,gender,email,phone,password):
-    print(os.path.realpath(os.path.dirname('vendor')))
-    db = connect_db()
-    db.execute('INSERT INTO user VALUES(name,birtday,gender,email,phone,password)')
-    return render_template('register.html')
+#@app.route('/register')
+#def reg(name,birtday,gender,email,phone,password):
+    #print(os.path.abspath(os.path.dirname('vendor')))
+    #db = connect_db()
+    #db.execute('INSERT INTO user VALUES(name,birtday,gender,email,phone,password)')
+    #return render_template('register.html')
